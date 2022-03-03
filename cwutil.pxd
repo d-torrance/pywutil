cc1f485da1fbc7f44b5fbdb714978bc712e588cc
Doug Torrance <dtorrance@piedmont.edu>
2022-03-03 12:25:49 -0500

    Initial commit -- WMArrays (mostly) working!

---
+++ cwutil.pxd
@@ -0,0 +1,29 @@
+cdef extern from "WINGs/WUtil.h":
+    ctypedef struct WMArray:
+        pass
+
+    WMArray* WMCreateArray(int initialSize)
+    # WMArray* WMCreateArrayWithDestructor(int initialSize,
+    #                                      WMFreeDataProc *destructor)
+    # WMArray* WMCreateArrayWithArray(WMArray *array)
+    void WMEmptyArray(WMArray *array)
+    void WMFreeArray(WMArray *array)
+    int WMGetArrayItemCount(WMArray *array)
+    void WMAppendArray(WMArray *array, WMArray *other)
+    void WMAddToArray(WMArray *array, void *item)
+    void WMInsertInArray(WMArray *array, int index, void *item)
+    void* WMReplaceInArray(WMArray *array, int index, void *item)
+    int WMDeleteFromArray(WMArray *array, int index)
+    # int WMRemoveFromArrayMatching(WMArray *array, WMMatchDataProc *match,
+    #                              void *cdata)
+    void* WMGetFromArray(WMArray *array, int index)
+    void* WMPopFromArray(WMArray *array)
+    # int WMFindInArray(WMArray *array, WMMatchDataProc *match, void *cdata)
+    int WMCountInArray(WMArray *array, void *item)
+    # void WMSortArray(WMArray *array, WMCompareDataProc *comparer)
+    # void WMMapArray(WMArray *array, void (*function)(void*, void*),
+    #                 void *data)
+    # WMArray* WMGetSubarrayWithRange(WMArray* array, WMRange aRange)
+    void* WMArrayFirst(WMArray *array, int *iter)
+    void* WMArrayNext(WMArray *array, int *iter)
+    # void* WMArrayPrevious(WMArray *array, WMArrayIterator *iter)
---
+++ wutil.pyx
@@ -0,0 +1,89 @@
+import collections.abc
+import cython
+from cpython.ref cimport PyObject
+cimport cwutil
+
+cdef class WMArray:
+    cdef cwutil.WMArray * _c_array
+
+    def __init__(self, iterable=None):
+        if iterable is None:
+            self._c_array = cwutil.WMCreateArray(0)
+        elif isinstance(iterable, collections.abc.Iterable):
+            self._c_array = cwutil.WMCreateArray(0)
+            for x in iterable:
+                self.append(x)
+        else:
+            raise TypeError(f"'{type(iterable).__name__}' not iterable")
+
+    def clear(self):
+        cwutil.WMEmptyArray(self._c_array)
+
+    def __dealloc__(self):
+        cwutil.WMFreeArray(self._c_array)
+
+    def __len__(self):
+        return cwutil.WMGetArrayItemCount(self._c_array)
+
+    def extend(self, WMArray other):
+        cwutil.WMAppendArray(self._c_array, other._c_array)
+
+    def append(self, item):
+        cwutil.WMAddToArray(self._c_array, <void *>item)
+
+    def insert(self, int index, item):
+        cwutil.WMInsertInArray(self._c_array, index, <void*>item)
+
+    def __setitem__(self, int index, item):
+        ret = cwutil.WMReplaceInArray(self._c_array, index, <void*>item)
+        if ret is cython.NULL:
+            raise IndexError("array index out of range")
+
+    def __delitem__(self, int index):
+        ret = cwutil.WMDeleteFromArray(self._c_array, index)
+        if ret == 0:
+            raise IndexError("array index out of range")
+
+    def __getitem__(self, int index):
+        ret = cwutil.WMGetFromArray(self._c_array, index)
+        if ret is cython.NULL:
+            raise IndexError("array index out of range")
+        else:
+            return <object>ret
+
+    def pop(self):
+        ret = cwutil.WMPopFromArray(self._c_array)
+        if ret is cython.NULL:
+            raise IndexError("pop from empty array")
+        else:
+            return <object>ret
+
+    def count(self, item):
+        return cwutil.WMCountInArray(self._c_array, <void *>item)
+
+    def __iter__(self):
+        return WMArrayIterator(self)
+
+    def __repr__(self):
+        return f"WMArray({list(self)})"
+
+cdef class WMArrayIterator:
+    cdef int index
+    cdef cwutil.WMArray *_c_array
+
+    def __init__(self, WMArray array):
+        self._c_array = array._c_array
+        self.index = -1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index == -1:
+            ret = cwutil.WMArrayFirst(self._c_array, &self.index)
+        else:
+            ret = cwutil.WMArrayNext(self._c_array, &self.index)
+        if ret is cython.NULL:
+            raise StopIteration
+        else:
+            return <object>ret
